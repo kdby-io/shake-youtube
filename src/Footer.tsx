346597490756aef515009f8c18a7a7681bc3ae4a
Yuna Heo <yunaheo@Yuna-2.local>
2023-08-01 01:05:48 +0900

    tailwind CSS Test

---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+
+const Footer = () => {
+    return (
+      <footer>
+        <p className="text-6xl text-blue-500">Tailwind CSS 테스트 </p>
+      </footer>
+    );
+  };
+
+  export default Footer
