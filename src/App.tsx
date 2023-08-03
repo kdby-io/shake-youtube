6a7c6836ccab7facb93f2fe0ca51d77a2e6f755e
sloth <dirtlrch@gmail.com>
2023-08-03 20:44:26 +0900

    Youtube api

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import { video } from "./services/youtube";
+import "./App.css";
 
-const useVideoId = () => new URLSearchParams(window.location.search).get('v')
+const useVideoId = () => new URLSearchParams(window.location.search).get("v");
 
 function App() {
-  const [count, setCount] = useState(0)
-  const videoId = useVideoId() ?? "SWqQQ6Yb-6g"
+  // const [count, setCount] = useState(0);
+  const videoId: string = useVideoId() ?? "SWqQQ6Yb-6g";
+  const [videoForShake, setvideoForShake] = useState<any>(null);
+
+  const getVideoInfo = async () => {
+    setvideoForShake(await video(videoId));
+  };
+
+  useEffect(() => {
+    getVideoInfo();
+  }, [videoId]);
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> */}
       <h1>Shake Youtube</h1>
       <p>http://localhost:5173/watch?v={videoId}</p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button> */}
+        <button onClick={() => console.log(videoForShake)}>
+          영상정보 콘솔에 출력하기
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoId}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+
+      <strong>타이틀</strong>
+      {videoForShake && <p>{videoForShake.title}</p>}
+      <div style={{ height: "30px" }}></div>
+      <strong>설명</strong>
+      {videoForShake && <pre>{videoForShake.description}</pre>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/youtube.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const YoutubeApiClient = axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3",
+  params: { key: import.meta.env.VITE_GOOGLE_API_KEY },
+});
+
+export const video = async (videoId: string) => {
+  try {
+    const response = await YoutubeApiClient.get("videos", {
+      params: {
+        part: "snippet",
+        id: videoId,
+      },
+    });
+    return response.data.items[0].snippet;
+  } catch (error) {
+    console.log(error);
+  }
+};
