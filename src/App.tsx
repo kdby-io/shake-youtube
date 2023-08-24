4e4a1c50968840e0a1afd3d0edbd2b99909d8be3
sloth <dirtlrch@gmail.com>
2023-08-24 11:11:31 +0900

    챕터 리스트 호버 추가, playing 챕터 클릭시 pause 동작 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { ChapterPlayLists } from "./components/ChapterPlayLists";
 import { Controller } from "./components/Controller";
 import { Title } from "./components/Title";
-import { shuffle } from "lodash"
+import { shuffle } from "lodash";
 
 const playerOpts: YouTubeProps["opts"] = {
   height: "390",
@@ -28,22 +28,27 @@ function App() {
 
   useEffect(() => {
     setShakedChapters(shuffle(chapters));
-  }, [chapters])
+  }, [chapters]);
 
   useEffect(() => {
     (async () => {
       const nextSecond = Math.ceil(playerTime);
-      const isAlmostEndChapter = startTimes.includes(nextSecond) && 0 < nextSecond - playerTime && nextSecond - playerTime <= 0.2;
+      const isAlmostEndChapter =
+        startTimes.includes(nextSecond) &&
+        0 < nextSecond - playerTime &&
+        nextSecond - playerTime <= 0.2;
 
       if (isAlmostEndChapter) {
-        const currentIndex = shakedChapters.findIndex((chapter) => chapter.end === Math.ceil(playerTime));
+        const currentIndex = shakedChapters.findIndex(
+          (chapter) => chapter.end === Math.ceil(playerTime)
+        );
         const isCurrentIndexLast = currentIndex === shakedChapters.length - 1;
         const nextIndex = isCurrentIndexLast ? 0 : currentIndex + 1;
         const nextChapter = shakedChapters[nextIndex];
         player?.seekTo(nextChapter.start, true);
       }
-    })()
-  },[playerTime])
+    })();
+  }, [playerTime]);
 
   const onPlayerReady: YouTubeProps["onReady"] = (event) => {
     setPlayer(event.target);
@@ -53,16 +58,18 @@ function App() {
     const newState = await player?.getPlayerState();
 
     if (newState === Youtube.PlayerState.ENDED) {
-        clearInterval(timer);
-        const currentIndex = shakedChapters.findIndex((chapter) => chapter.end === Infinity);
-        const isCurrentIndexLast = currentIndex === shakedChapters.length - 1;
-        const nextIndex = isCurrentIndexLast ? 0 : currentIndex + 1;
-        const nextChapter = shakedChapters[nextIndex];
-        player?.seekTo(nextChapter.start, true);
+      clearInterval(timer);
+      const currentIndex = shakedChapters.findIndex(
+        (chapter) => chapter.end === Infinity
+      );
+      const isCurrentIndexLast = currentIndex === shakedChapters.length - 1;
+      const nextIndex = isCurrentIndexLast ? 0 : currentIndex + 1;
+      const nextChapter = shakedChapters[nextIndex];
+      player?.seekTo(nextChapter.start, true);
     } else if (newState === Youtube.PlayerState.PLAYING) {
       setNowPlaying(true);
       timer = setInterval(async () => {
-        const time = await player?.getCurrentTime() ?? 0;
+        const time = (await player?.getCurrentTime()) ?? 0;
         setPlayerTime(time);
         console.log(time);
       }, 200);
@@ -74,9 +81,13 @@ function App() {
     }
   };
 
-  const handleChapterClick = (s: number) => {
-    player?.seekTo(s, true);
-    player?.playVideo();
+  const handleChapterClick = (s: number, playing: boolean) => {
+    if (playing) {
+      player?.pauseVideo();
+    } else {
+      player?.seekTo(s, true);
+      player?.playVideo();
+    }
   };
 
   const handlePlayButtonClick = () => {
--- src/components/ChapterPlayLists.tsx
+++ src/components/ChapterPlayLists.tsx
@@ -1,8 +1,9 @@
 import { Chapter } from "../services/youtube";
+import playing_icon from "../assets/playing_icon.png";
 
 type Props = {
   chapters: Chapter[];
-  onClick: (moveTo: number) => void;
+  onClick: (moveTo: number, palying: boolean) => void;
   playerTime: number | undefined;
 };
 export const ChapterPlayLists = ({ chapters, onClick, playerTime }: Props) => {
@@ -10,14 +11,20 @@ export const ChapterPlayLists = ({ chapters, onClick, playerTime }: Props) => {
     <div className="pt-12">
       <ul role="list" className="divide-y divide-gray-800">
         {chapters.map((item) => {
-          const playing = playerTime ? item.start <= playerTime && playerTime <= item.end : false;
+          const playing = playerTime
+            ? item.start <= playerTime && playerTime <= item.end
+            : false;
           return (
             <li
               key={item.title}
-              className="flex pl-4 py-6 cursor-pointer"
-              onClick={() => onClick(item.start)}
+              className={
+                playing
+                  ? "flex pl-4 py-6 cursor-pointer playing"
+                  : "flex pl-4 py-6 cursor-pointer not-playing"
+              }
+              onClick={() => onClick(item.start, playing)}
             >
-              <ChapterPlayListItem item={item} playing={playing} />
+              <ChapterPlayListItem item={item} />
             </li>
           );
         })}
@@ -26,20 +33,17 @@ export const ChapterPlayLists = ({ chapters, onClick, playerTime }: Props) => {
   );
 };
 
-const ChapterPlayListItem = ({
-  item,
-  playing,
-}: {
-  item: Chapter;
-  playing: boolean;
-}) => {
+const ChapterPlayListItem = ({ item }: { item: Chapter }) => {
   return (
     <>
-      {playing ? (
-        <div className="text-base text-white">{`(요고재생중) ${item.title}`}</div>
-      ) : (
-        <div className="text-base text-gray-500">{item.title}</div>
-      )}
+      <div className="text-base">
+        <img
+          src={playing_icon}
+          alt="playing_icon"
+          className="playing_icon pr-4"
+        ></img>
+        {item.title}
+      </div>
     </>
   );
 };
