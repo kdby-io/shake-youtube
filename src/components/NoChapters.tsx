008f20d1cae2c2d473825573422639d988ef9dfe
sloth <dirtlrch@gmail.com>
2023-09-13 13:32:54 +0900

    NoChapters 표현 영어로 변경

--- src/components/NoChapters.tsx
+++ src/components/NoChapters.tsx
@@ -8,12 +8,12 @@ export const NoChapters = ({ videoId, className }: Props) => {
   return (
     <div className={className}>
       <div className=" text-center text-base text-[#8A9397]">
-        <p>해당 플레이리스트에 타임라인이 없어요!</p>
-        <p>댓글에 타임라인을 달아주세요.</p>
+        <p>This video doesn't have a playlist!</p>
+        <p>Please leave comment with timestamps to the video.</p>
       </div>
       <div className=" bg-[#1A1B21] py-11 rounded-2xl flex flex-col items-center gap-7">
         <div>
-          <p>타임라인 추가 방법</p>
+          <p>How to add timestamps</p>
           <img src={no_chapters} className=" mt-4"></img>
         </div>
         <a
@@ -21,7 +21,9 @@ export const NoChapters = ({ videoId, className }: Props) => {
           target="_blank"
           className="text-[#ffffff]"
         >
-          <button className=" hover:text-[#ffffff]">타임라인 추가하기</button>
+          <button className=" hover:text-[#ffffff]">
+            Leave a comment &gt;
+          </button>
         </a>
       </div>
     </div>
